637e3eaf0e76b8ff92004ee4e0c2522b265a75e1
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-08-12 23:36:48 +0530

    Add student marks and pass/fail logic to Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,21 @@
 public class Test {
     public static void main(String[] args) {
-    int a=500;
-    int b=30;
-    int c =a+b;
-    int d =a-b;
-        System.out.println("*******************************************");
+    int MATH =97;
+    int PHYSIC =90;
+    int CHEMISTRY =80;
+    int TOTAL  = MATH+PHYSIC+CHEMISTRY;
+System.out.println("********STUDENT INFORMATION ********************");
         
-    System.out.println("the addition of A and B "+c);        
-    System.out.println("the addition of A and B "+d);
+System.out.println("The Marks in MATH "+MATH);        
+System.out.println("The Marks in PHYSICS"+PHYSIC);
+System.out.println("The Marks in CHEMISTRY "+CHEMISTRY);
+        if(TOTAL/3 >=35%) {
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAIL")
         
-     System.out.println("*******************************************");
+ System.out.println("************CONGRASS**************************");
     
     
     }}
